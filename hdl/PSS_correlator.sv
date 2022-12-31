3658517dcb27626be26c73d8b4e6d55f61f701e8
Benjamin Menküc <benjamin@menkuec.de>
2022-12-31 20:03:22 +0100

    WIP

--- hdl/PSS_correlator.sv
+++ hdl/PSS_correlator.sv
@@ -16,14 +16,19 @@ module PSS_correlator
     output  reg                                 m_axis_out_tvalid
 );
 
+reg signed [15:0] tap_re, tap_im;
+
 initial begin
     for (integer i=0; i<10; i=i+1) begin
-        $display("PSS_LOCAL[%d] = %d + j%d", i, PSS_LOCAL[i*32+:16], PSS_LOCAL[i*32+16+:16]);
+        tap_re = PSS_LOCAL[i*32+:16];
+        tap_im = PSS_LOCAL[i*32+16+:16];
+        $display("PSS_LOCAL[%d] = %d + j%d", i, tap_re, tap_im);
     end
 end
 
-reg signed [IN_DW-1:0] in_ff [0:127];
-reg signed [IN_DW-1:0] multiplied [0:127];
+reg signed [IN_DW/2-1:0] in_re [0:PSS_LEN-1];
+reg signed [IN_DW/2-1:0] in_im [0:PSS_LEN-1];
+reg signed [IN_DW-1:0] multiplied [0:PSS_LEN-1];
 reg valid;
 
 always @(posedge clk_i) begin // cannot use $display inside always_ff
@@ -32,37 +37,43 @@ always @(posedge clk_i) begin // cannot use $display inside always_ff
         m_axis_out_tvalid <= '0;
         valid <= '0;
         for (integer i=0; i<PSS_LEN; i++) begin
-            in_ff[i] <= '0;
+            in_re[i] <= '0;
+            in_im[i] <= '0;
         end        
     end
     else begin
         if (s_axis_in_tvalid) begin
-            in_ff[0] <= s_axis_in_tdata;
+            in_re[0] <= s_axis_in_tdata[15:0];
+            in_im[0] <= s_axis_in_tdata[31:16];
             for (integer i=0; i<PSS_LEN; i++) begin
-                in_ff[i+1] <= in_ff[i];
+                in_re[i+1] <= in_re[i];
+                in_im[i+1] <= in_im[i];
             end
-            // $display("in_ff[0] = %d + j%d", in_ff[0][IN_DW/2:0], in_ff[0][IN_DW-1-:IN_DW/2]);
+            // $display("in[0] = %d + j%d", in_re[0], in_re[0]);
             valid <= 1'b1;
         end else begin
             valid <= '0;
         end
 
-        m_axis_out_tvalid <= valid;
         if (valid) begin
             m_axis_out_tdata <= sum_im*sum_im + sum_re*sum_re;
+            m_axis_out_tvalid <= '1;
         end else begin
             m_axis_out_tdata <= '0;
+            m_axis_out_tvalid <= '0;
         end
     end
 end
 
 reg signed [15:0] sum_im, sum_re;
 always_comb begin
-    sum_im = 16'd0;
-    sum_re = 16'd0;
+    sum_im = '0;
+    sum_re = '0;
     for (integer i=0; i<PSS_LEN; i++) begin
-        sum_im = sum_im + in_ff[i][IN_DW/2-1:0] * PSS_LOCAL[i*32+16+:16];
-        sum_re = sum_re + in_ff[i][IN_DW-1-:IN_DW/2] * PSS_LOCAL[i*32+:16];
+        tap_re = PSS_LOCAL[i*32+:16];
+        tap_im = PSS_LOCAL[i*32+16+:16];        
+        sum_im = sum_im + in_re[i] * tap_im + in_im[i] * tap_re;
+        sum_re = sum_re - in_im[i] * tap_im + in_re[i] * tap_re;
     end
 end
 
--- tests/test_PSS_correlator.py
+++ tests/test_PSS_correlator.py
@@ -1,9 +1,11 @@
 import numpy as np
+import scipy
 import os
 import pytest
 import logging
 import importlib
-#import matplotlib.pyplot as plt
+import sys
+import matplotlib.pyplot as plt
 
 import cocotb
 import cocotb_test.simulator
@@ -11,7 +13,8 @@ from cocotb.clock import Clock
 from cocotb.triggers import Timer
 from cocotb.triggers import RisingEdge
 
-#import py3gpp
+import py3gpp
+import sigmf
 
 CLK_PERIOD_NS = 8
 CLK_PERIOD_S = CLK_PERIOD_NS * 0.000000001
@@ -27,8 +30,8 @@ class TB(object):
         self.log.setLevel(logging.DEBUG)
 
         tests_dir = os.path.abspath(os.path.dirname(__file__))
-        model_dir = os.path.abspath(os.path.join(tests_dir, "../model/PSS_correlator"))
-        spec = importlib.util.spec_from_file_location("PSS_correlator", model_dir)
+        model_dir = os.path.abspath(os.path.join(tests_dir, '../model/PSS_correlator.py'))
+        spec = importlib.util.spec_from_file_location('PSS_correlator', model_dir)
         foo = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(foo)
         self.model = foo.Model() 
@@ -53,23 +56,39 @@ class TB(object):
         await RisingEdge(self.dut.clk_i)
         self.dut.reset_ni.value = 1
         await RisingEdge(self.dut.clk_i)
-        self.model.reset()    
+        self.model.reset()
 
 @cocotb.test()
 async def simple_test(dut):
+    handle = sigmf.sigmffile.fromfile('../../tests/30720KSPS_dl_signal.sigmf-data')
+    waveform = handle.read_samples()
+    print(len(waveform))
+    waveform = scipy.signal.decimate(waveform, 8, ftype='fir')
+    print(len(waveform))
+    waveform /= max(waveform.real.max(), waveform.imag.max())
+    waveform *= 2**15
+
     tb = TB(dut)
     await tb.cycle_reset()
 
-    num_items = 100
+    num_items = 1000
     i = 0
+    in_counter = 0
+    received = np.empty(num_items, int)
     while i < num_items:
         await RisingEdge(dut.clk_i)
-        dut.s_axis_in_tdata.value = 1 + (2<<16)
+        # dut.s_axis_in_tdata.value = 1 + (2<<16)
+        dut.s_axis_in_tdata.value = ((int(waveform[in_counter].imag)&0xFFFF)<<16) + ((int(waveform[in_counter].real))&0xFFFF)
         dut.s_axis_in_tvalid.value = 1
+        in_counter += 1
 
         if dut.m_axis_out_tvalid == 1:
-            print(dut.m_axis_out_tdata.value.integer)
-        i  += 1
+            # print(dut.m_axis_out_tdata.value.integer)
+            received[i] = dut.m_axis_out_tdata.value.integer
+            i  += 1
+
+    plt.plot(np.sqrt(received))
+    plt.show()
 
 def test():
     dut = 'PSS_correlator'
@@ -88,17 +107,18 @@ def test():
     parameters['PSS_LEN'] = PSS_LEN
 
     # imaginary part is in upper 16 Bit
-    #py3gpp.nrPSS(2)
-    taps = np.zeros(10, 'complex')
-#    taps = np.fft.ifft(py3gpp.nrPSS(2)) * np.sqrt(PSS_LEN) * 2**15
-    #taps[0] = 1 + 1j*10
-    #taps[1] = 2 + 1j*20
+    #taps = np.zeros(PSS_LEN, 'complex')
+    taps = np.fft.ifft(py3gpp.nrPSS(2)) * np.sqrt(PSS_LEN) * 2**15
+    taps[0] = 1 + 1j*10
+    taps[1] = -2 - 1j*20
     parameters['PSS_LOCAL'] = 0
     for i in range(len(taps)):
-        parameters['PSS_LOCAL'] += (np.imag(taps[i]).astype(int) << (32*i + 16)) + (np.real(taps[i]).astype(int)<<32*i)
-
+        parameters['PSS_LOCAL'] += ((int(np.imag(taps[i]))&0xFFFF) << (32*i + 16)) + ((int(np.real(taps[i]))&0xFFFF) << (32*i))
+    sys.set_int_max_str_digits(100000)        
     extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
-    sim_build='sim_build/' + '_'.join(('{}={}'.format(*i) for i in parameters.items()))
+    parameters_no_taps = parameters.copy()
+    del parameters_no_taps['PSS_LOCAL']
+    sim_build='sim_build/' + '_'.join(('{}={}'.format(*i) for i in parameters_no_taps.items()))
     cocotb_test.simulator.run(
         python_search=[tests_dir],
         verilog_sources=verilog_sources,
