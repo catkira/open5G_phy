1c959a11118d4583e83978ebbc75742a1ade61c4
Benjamin Menkuec <benjamin@menkuec.de>
2023-02-07 22:40:59 +0100

    started to implement CFO correction

---
+++ hdl/test_CFO_correction.sv
@@ -0,0 +1,169 @@
+`timescale 1ns / 1ns
+
+module test_CFO_correction
+#(
+    parameter IN_DW = 32,           // input data width
+    parameter OUT_DW = 32,          // correlator output data width
+    parameter TAP_DW = 32,
+    parameter PSS_LEN = 128,
+    parameter [TAP_DW * PSS_LEN - 1 : 0] PSS_LOCAL = {(PSS_LEN * TAP_DW){1'b0}},
+    parameter ALGO = 0,
+    parameter WINDOW_LEN = 8,
+    parameter MULT_REUSE = 16    
+)
+(
+    input                                       clk_i,
+    input                                       reset_ni,
+    input   wire           [IN_DW-1:0]          s_axis_in_tdata,
+    input                                       s_axis_in_tvalid,
+    
+    output  wire            [IN_DW + TAP_DW + 2 + 2 * $clog2(PSS_LEN) - 1 : 0]   C0,
+    output  wire            [IN_DW + TAP_DW + 2 + 2 * $clog2(PSS_LEN) - 1: 0]    C1,
+
+    output  reg                                 peak_detected_o,
+    
+    // debug outputs
+    output  wire            [IN_DW-1:0]         m_axis_cic_debug_tdata,
+    output  wire                                m_axis_cic_debug_tvalid,
+    output  wire           [OUT_DW - 1 : 0]     m_axis_correlator_debug_tdata,
+    output  wire                                m_axis_correlator_debug_tvalid
+);
+
+wire [IN_DW - 1 : 0] m_axis_cic_tdata;
+wire                 m_axis_cic_tvalid;
+assign m_axis_cic_debug_tdata = m_axis_cic_tdata;
+assign m_axis_cic_debug_tvalid = m_axis_cic_tvalid;
+
+wire [31 : 0] DDS_out;
+wire DDS_out_valid;
+
+reg [19 : 0] DDS_phase;
+reg  DDS_phase_valid;
+reg [31 : 0] mult_out_tdata;
+reg          mult_out_tvalid;
+
+always @(posedge clk_i) begin
+    if (!reset_ni) begin
+        DDS_phase <= '0;
+        DDS_phase_valid <= '0;
+    end else begin
+        DDS_phase <= '0;
+        DDS_phase_valid <= 1;
+    end
+end
+
+dds #(
+    .PHASE_DW(20),
+    .OUT_DW(16),
+    .USE_TAYLOR(1),
+    .LUT_DW(16),
+    .SIN_COS(1),
+    .NEGATIVE_SINE(0),
+    .NEGATIVE_COSINE(0)
+)
+dds_i(
+    .clk(clk_i),
+    .reset_n(reset_ni),
+    .s_axis_phase_tdata(DDS_phase),
+    .s_axis_phase_tvalid(DDS_phase_valid),
+
+    .m_axis_out_tdata(DDS_out),
+    .m_axis_out_tvalid(DDS_out_valid)
+);
+
+complex_multiplier #(
+    .OPERAND_WIDTH_A(16),
+    .OPERAND_WIDTH_B(IN_DW/2),
+    .OPERAND_WIDTH_OUT(IN_DW),
+    .BLOCKING(0)
+)
+complex_multiplier_i(
+    .aclk(clk_i),
+    .aresetn(reset_ni),
+    .s_axis_a_tdata(DDS_out),
+    .s_axis_a_tvalid(DDS_out_valid),
+    .s_axis_b_tdata(s_axis_in_tdata),
+    .s_axis_b_tvalid(s_axis_in_tvalid),
+
+    .m_axis_dout_tdata(mult_out_tdata),
+    .m_axis_dout_tvalid(mult_out_tvalid)
+);
+
+cic_d #(
+    .INP_DW(IN_DW/2),
+    .OUT_DW(IN_DW/2),
+    .CIC_R(2),
+    .CIC_N(3),
+    .VAR_RATE(0)
+)
+cic_real(
+    .clk(clk_i),
+    .reset_n(reset_ni),
+    .s_axis_in_tdata(mult_out_tdata[IN_DW / 2 - 1 -: IN_DW / 2]),
+    .s_axis_in_tvalid(mult_out_tvalid),
+    .m_axis_out_tdata(m_axis_cic_tdata[IN_DW / 2 - 1 -: IN_DW / 2]),
+    .m_axis_out_tvalid(m_axis_cic_tvalid)
+);
+
+cic_d #(
+    .INP_DW(IN_DW / 2),
+    .OUT_DW(IN_DW / 2),
+    .CIC_R(2),
+    .CIC_N(3),
+    .VAR_RATE(0)
+)
+cic_imag(
+    .clk(clk_i),
+    .reset_n(reset_ni),
+    .s_axis_in_tdata(mult_out_tdata[IN_DW - 1 -: IN_DW / 2]),
+    .s_axis_in_tvalid(mult_out_tvalid),
+    .m_axis_out_tdata(m_axis_cic_tdata[IN_DW - 1 -: IN_DW / 2])
+);
+
+
+wire [OUT_DW - 1 : 0] correlator_tdata;
+wire correlator_tvalid;
+assign m_axis_correlator_debug_tdata = correlator_tdata;
+assign m_axis_correlator_debug_tvalid = correlator_tvalid;
+
+PSS_correlator_mr #(
+    .IN_DW(IN_DW),
+    .OUT_DW(OUT_DW),
+    .TAP_DW(TAP_DW),
+    .PSS_LEN(PSS_LEN),
+    .PSS_LOCAL(PSS_LOCAL),
+    .ALGO(ALGO),
+    .MULT_REUSE(MULT_REUSE)
+)
+correlator(
+    .clk_i(clk_i),
+    .reset_ni(reset_ni),
+    .s_axis_in_tdata(m_axis_cic_tdata),
+    .s_axis_in_tvalid(m_axis_cic_tvalid),
+    .m_axis_out_tdata(correlator_tdata),
+    .m_axis_out_tvalid(correlator_tvalid),
+    .C0(C0),
+    .C1(C1)
+);
+
+Peak_detector #(
+    .IN_DW(OUT_DW),
+    .WINDOW_LEN(WINDOW_LEN)
+)
+peak_detector(
+    .clk_i(clk_i),
+    .reset_ni(reset_ni),
+    .s_axis_in_tdata(correlator_tdata),
+    .s_axis_in_tvalid(correlator_tvalid),
+    .peak_detected_o(peak_detected_o)
+);
+
+`ifdef COCOTB_SIM
+initial begin
+  $dumpfile ("debug.vcd");
+  $dumpvars (0, test_CFO_correction);
+  // #1;
+end
+`endif
+
+endmodule
---
+++ tests/test_CFO_correction.py
@@ -0,0 +1,247 @@
+import numpy as np
+import scipy
+import os
+import pytest
+import logging
+import importlib
+import matplotlib.pyplot as plt
+import os
+
+import cocotb
+import cocotb_test.simulator
+from cocotb.clock import Clock
+from cocotb.triggers import Timer
+from cocotb.triggers import RisingEdge
+
+import py3gpp
+import sigmf
+
+CLK_PERIOD_NS = 8
+CLK_PERIOD_S = CLK_PERIOD_NS * 0.000000001
+tests_dir = os.path.abspath(os.path.dirname(__file__))
+rtl_dir = os.path.abspath(os.path.join(tests_dir, '..', 'hdl'))
+
+def _twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0:
+        val = val - (1 << bits)
+    return int(val)
+
+class TB(object):
+    def __init__(self, dut):
+        self.dut = dut
+        self.IN_DW = int(dut.IN_DW.value)
+        self.OUT_DW = int(dut.OUT_DW.value)
+        self.TAP_DW = int(dut.TAP_DW.value)
+        self.PSS_LEN = int(dut.PSS_LEN.value)
+        self.PSS_LOCAL = int(dut.PSS_LOCAL.value)
+        self.ALGO = int(dut.ALGO.value)
+        self.MULT_REUSE = int(dut.MULT_REUSE.value)
+
+        self.log = logging.getLogger('cocotb.tb')
+        self.log.setLevel(logging.DEBUG)
+
+        tests_dir = os.path.abspath(os.path.dirname(__file__))
+        model_dir = os.path.abspath(os.path.join(tests_dir, '../model/PSS_correlator.py'))
+        spec = importlib.util.spec_from_file_location('PSS_correlator', model_dir)
+        foo = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(foo)
+        self.model = foo.Model(self.IN_DW, self.OUT_DW, self.TAP_DW, self.PSS_LEN, self.PSS_LOCAL, self.ALGO)
+
+        cocotb.start_soon(Clock(self.dut.clk_i, CLK_PERIOD_NS, units='ns').start())
+        cocotb.start_soon(self.model_clk(CLK_PERIOD_NS, 'ns'))
+
+    async def model_clk(self, period, period_units):
+        timer = Timer(period, period_units)
+        while True:
+            self.model.tick()
+            await timer
+
+    async def generate_input(self):
+        pass
+
+    async def cycle_reset(self):
+        self.dut.s_axis_in_tvalid.value = 0
+        self.dut.reset_ni.setimmediatevalue(1)
+        await RisingEdge(self.dut.clk_i)
+        self.dut.reset_ni.value = 0
+        await RisingEdge(self.dut.clk_i)
+        self.dut.reset_ni.value = 1
+        await RisingEdge(self.dut.clk_i)
+        self.model.reset()
+
+@cocotb.test()
+async def simple_test(dut):
+    tb = TB(dut)
+    handle = sigmf.sigmffile.fromfile('../../tests/30720KSPS_dl_signal.sigmf-data')
+    waveform = handle.read_samples()
+    fs = 30720000
+    CFO = int(os.getenv('CFO'))
+    print(f'CFO = {CFO} Hz')
+    waveform *= np.exp(np.arange(len(waveform))*1j*2*np.pi*CFO/fs)
+    waveform /= max(waveform.real.max(), waveform.imag.max())
+    decimation_factor = 16
+    waveform = scipy.signal.decimate(waveform, decimation_factor, ftype='fir')
+    waveform /= max(waveform.real.max(), waveform.imag.max())
+    waveform *= 2 ** (tb.IN_DW // 2 - 1) - 1
+    waveform = waveform.real.astype(int) + 1j*waveform.imag.astype(int)
+    await tb.cycle_reset()
+
+    num_items = 500
+    rx_counter = 0
+    rx_counter_model = 0
+    in_counter = 0
+    received = np.empty(num_items, int)
+    received_model = np.empty(num_items, int)
+    clk_div = 0
+    clk_decimation = 16
+    C0 = []
+    C1 = []
+    C_DW = int(tb.IN_DW + tb.TAP_DW + 2 + 2*np.ceil(np.log2(tb.PSS_LEN)))
+    while rx_counter < num_items:
+        await RisingEdge(dut.clk_i)
+        if clk_div < (clk_decimation - 1):
+            dut.s_axis_in_tvalid.value = 0
+            clk_div += 1
+        else:
+            clk_div = 0
+            data = (((int(waveform[in_counter].imag)  & (2 ** (tb.IN_DW // 2) - 1)) << (tb.IN_DW // 2)) \
+                  + ((int(waveform[in_counter].real)) & (2 ** (tb.IN_DW // 2) - 1))) & (2 ** tb.IN_DW - 1)
+            dut.s_axis_in_tdata.value = data
+            dut.s_axis_in_tvalid.value = 1
+            tb.model.set_data(data)
+            in_counter += 1
+
+        if dut.m_axis_correlator_debug_tvalid.value.integer == 1:
+            received[rx_counter] = dut.m_axis_correlator_debug_tdata.value.integer
+            C0.append(_twos_comp(dut.C0.value.integer & (2 ** (C_DW // 2) - 1),C_DW // 2) \
+                    + 1j * _twos_comp((dut.C0.value.integer >> (C_DW // 2)) & (2 ** (C_DW // 2) - 1), C_DW // 2))
+            C1.append(_twos_comp(dut.C1.value.integer & (2 ** (C_DW // 2) - 1), C_DW // 2) \
+                    + 1j * _twos_comp((dut.C1.value.integer >> (C_DW // 2)) & (2 ** (C_DW // 2) - 1), C_DW // 2))
+            rx_counter  += 1
+
+        if tb.model.data_valid() and rx_counter_model < num_items:
+            received_model[rx_counter_model] = tb.model.get_data()
+            # print(f'{rx_counter_model}: rx mod {received_model[rx_counter_model]}')
+            rx_counter_model += 1
+
+    ssb_start = np.argmax(received) - 128
+    received = np.array(received)[128:]
+    received_model = np.array(received_model)[128:]
+    print(f'max model {max(received_model)} max hdl {max(received)}')
+    if 'PLOTS' in os.environ and os.environ['PLOTS'] == '1':
+        _, (ax, ax2) = plt.subplots(2, 1)
+        print(f'{type(received.dtype)} {type(received_model.dtype)}')
+        ax.plot(np.sqrt(received))
+        ax2.plot(np.sqrt(received_model), 'r-')
+        ax.axvline(x = ssb_start, color = 'y', linestyle = '--', label = 'axvline - full height')
+        plt.show()
+    print(f'max correlation is {received[ssb_start]} at {ssb_start}')
+
+    print(f'max model-hdl difference is {max(np.abs(received - received_model))}')
+    if tb.ALGO == 0:
+        for i in range(len(received)):
+            assert received[i] == received_model[i]
+    else:
+        # TODO: implement model
+        pass
+    if tb.ALGO == 0:
+        prod = C0[ssb_start+128] * np.conj(C1[ssb_start+128])
+        # detectedCFO = np.arctan2(prod.imag, prod.real)
+        detectedCFO = np.angle(prod)
+        detectedCFO_Hz = detectedCFO / (2*np.pi) * (fs/decimation_factor) / 64
+        print(f'detected CFO = {detectedCFO_Hz} Hz')
+    else:
+        print('CFO estimation is not possible with ALGO=1')
+    
+    PSS = np.zeros(128, 'complex')
+    PSS[:-1] = py3gpp.nrPSS(2)
+    taps = np.fft.ifft(np.fft.fftshift(PSS))
+    C0 = np.vdot(waveform[ssb_start:][:64], taps[:64])
+    C1 = np.vdot(waveform[ssb_start+64:][:64], taps[64:][:64])
+    prod = C0 * np.conj(C1)
+    # detectedCFO = np.arctan2(prod.imag, prod.real)
+    detectedCFO = np.angle(prod)
+    detectedCFO_Hz_model = detectedCFO / (2*np.pi) * (fs/decimation_factor) / 64
+    print(f'detected CFO (model) = {detectedCFO_Hz_model} Hz')
+    if tb.ALGO == 0:
+        if CFO != 0:
+            assert (np.abs(detectedCFO_Hz - detectedCFO_Hz_model)/np.abs(CFO)) < 0.05
+        else:
+            assert np.abs(detectedCFO_Hz - detectedCFO_Hz_model) < 20
+
+    assert ssb_start == 284
+    assert len(received) == num_items - 128
+
+# bit growth inside PSS_correlator is a lot, be careful to not make OUT_DW too small !
+@pytest.mark.parametrize("ALGO", [0])
+@pytest.mark.parametrize("IN_DW", [32])
+@pytest.mark.parametrize("OUT_DW", [45])
+@pytest.mark.parametrize("TAP_DW", [32])
+@pytest.mark.parametrize("CFO", [0, 6500, -5500])
+@pytest.mark.parametrize("MULT_REUSE", [1, 16])
+def test(IN_DW, OUT_DW, TAP_DW, ALGO, CFO, MULT_REUSE):
+    dut = 'test_CFO_correction'
+    module = os.path.splitext(os.path.basename(__file__))[0]
+    toplevel = dut
+
+    verilog_sources = [
+        os.path.join(rtl_dir, f'{dut}.sv'),
+        os.path.join(rtl_dir, 'Peak_detector.sv'),
+        os.path.join(rtl_dir, 'PSS_correlator_mr.sv'),
+        os.path.join(rtl_dir, 'CIC/cic_d.sv'),
+        os.path.join(rtl_dir, 'CIC/comb.sv'),
+        os.path.join(rtl_dir, 'CIC/downsampler.sv'),
+        os.path.join(rtl_dir, 'CIC/integrator.sv'),
+        os.path.join(rtl_dir, 'complex_multiplier', 'complex_multiplier.v'),
+        os.path.join(rtl_dir, 'DDS', 'dds.sv')
+    ]
+    includes = [
+        os.path.join(rtl_dir, 'CIC')        
+    ]
+
+    PSS_LEN = 128
+    parameters = {}
+    parameters['IN_DW'] = IN_DW
+    parameters['OUT_DW'] = OUT_DW
+    parameters['TAP_DW'] = TAP_DW
+    parameters['PSS_LEN'] = PSS_LEN
+    parameters['ALGO'] = ALGO
+    parameters['MULT_REUSE'] = MULT_REUSE
+
+    # imaginary part is in upper 16 Bit
+    PSS = np.zeros(PSS_LEN, 'complex')
+    PSS[0:-1] = py3gpp.nrPSS(2)
+    taps = np.fft.ifft(np.fft.fftshift(PSS))
+    taps /= max(taps.real.max(), taps.imag.max())
+    taps *= 2 ** (TAP_DW // 2 - 1) - 1
+    # for i in range(10):
+    #     print(f'taps[{i}] = {taps[i]}')
+    parameters['PSS_LOCAL'] = 0
+    for i in range(len(taps)):
+        parameters['PSS_LOCAL'] += ((int(np.imag(taps[i])) & (2 ** (TAP_DW // 2) - 1)) << (TAP_DW * i + TAP_DW // 2)) \
+                                +  ((int(np.real(taps[i])) & (2 ** (TAP_DW // 2) - 1)) << (TAP_DW * i))
+    extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
+    os.environ['CFO'] = str(CFO)
+    parameters_no_taps = parameters.copy()
+    del parameters_no_taps['PSS_LOCAL']
+    sim_build='sim_build/' + '_'.join(('{}={}'.format(*i) for i in parameters_no_taps.items()))
+    cocotb_test.simulator.run(
+        python_search=[tests_dir],
+        verilog_sources=verilog_sources,
+        includes=includes,
+        toplevel=toplevel,
+        module=module,
+        # defines = {"LUT_PATH":"tests"},
+        parameters=parameters,
+        sim_build=sim_build,
+        extra_env=extra_env,
+        testcase='simple_test',
+        compile_args = ['-DLUT_PATH=\"../../tests\"'],
+        force_compile=True
+    )
+
+if __name__ == '__main__':
+    os.environ['PLOTS'] = "1"
+    # this setup does not require output truncation
+    test(IN_DW = 32, OUT_DW = 48, TAP_DW = 18, ALGO = 0, CFO = 6500, MULT_REUSE = 16)
