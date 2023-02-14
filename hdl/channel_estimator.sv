eff6793cae6907ce08421c50b091062d8ad289e2
Benjamin Menkuec <benjamin@menkuec.de>
2023-02-14 07:30:30 +0100

    added generation of all 8 PBCH DMRS sequences for a given N_id

--- hdl/channel_estimator.sv
+++ hdl/channel_estimator.sv
@@ -29,12 +29,10 @@ reg [3 : 0] state_PBCH_DMRS;
 localparam PBCH_DMRS_LEN = 144;
 reg [2 : 0] PBCH_DMRS [0 : PBCH_DMRS_LEN - 1];
 reg [$clog2(1600) : 0] PBCH_DMRS_cnt;
-reg [2 : 0] ibar_SSB;
 
 localparam LFSR_N = 31;
 reg lfsr_out_0, lfsr_out_1;
 reg lfsr_valid;
-reg [LFSR_N - 1 : 0] c_init;
 reg LFSR_reset_n;
 reg LFSR_load_config;
 
@@ -44,7 +42,7 @@ LFSR #(
     .START_VALUE(1),
     .VARIABLE_CONFIG(1)
 )
-lfsr_0
+lfsr_0_i
 (
     .clk_i(clk_i),
     .reset_ni(LFSR_reset_n),
@@ -54,21 +52,43 @@ lfsr_0
     .data_o(lfsr_out_0),
     .valid_o(lfsr_valid)
 );
-LFSR #(
-    .N(LFSR_N),
-    .TAPS('b1001),
-    .START_VALUE(1),
-    .VARIABLE_CONFIG(1)
-)
-lfsr_1
-(
-    .clk_i(clk_i),
-    .reset_ni(LFSR_reset_n),
-    .load_config_i(LFSR_load_config),
-    .taps_i('b1111),
-    .start_value_i(c_init),
-    .data_o(lfsr_out_1)
-);
+
+genvar ii;
+for (ii = 0; ii < 8; ii = ii + 1) begin : LFSR_1
+
+    reg [LFSR_N - 1 : 0] c_init;
+    reg out;
+    localparam [2 : 0] ibar_SSB = ii;
+    
+    LFSR #(
+        .N(LFSR_N),
+        .TAPS('b1001),
+        .START_VALUE(1),
+        .VARIABLE_CONFIG(1)
+    )
+    lfsr_i
+    (
+        .clk_i(clk_i),
+        .reset_ni(LFSR_reset_n),
+        .load_config_i(LFSR_load_config),
+        .taps_i('b1111),
+        .start_value_i(c_init),
+        .data_o(out)
+    );
+
+    always @(posedge clk_i) begin
+        if (!reset_ni) begin
+        end else begin
+            if (N_id_valid_i) begin
+                N_id_used <= N_id_i;
+                c_init = (((ibar_SSB + 1) * ((N_id_i >> 2) + 1)) << 11) +  ((ibar_SSB + 1) << 6) + (N_id_i[1 : 0] % 4);
+                $display("cinit = %x", c_init);
+            end            
+        end
+    end
+end
+
+
 
 always @(posedge clk_i) begin
     if (!reset_ni) begin
@@ -95,15 +115,10 @@ always @(posedge clk_i) begin
         LFSR_load_config <= '0;
         debug_PBCH_DMRS_o <= '0;
         debug_PBCH_DMRS_valid_o <= '0;
-        ibar_SSB <= 3'b010; // TODO: make this variable
     end else begin
         case (state_PBCH_DMRS)
             0: begin
                 if (N_id_valid_i) begin
-                    N_id_used <= N_id_i;
-                    ibar_SSB <= 3'b010;
-                    c_init = (((ibar_SSB + 1) * ((N_id_i >> 2) + 1)) << 11) +  ((ibar_SSB + 1) << 6) + (N_id_i[1 : 0] % 4);
-                    $display("cinit = %x", c_init);
                     state_PBCH_DMRS <= 1;
                     PBCH_DMRS_cnt <= '0;
                     LFSR_reset_n <= '1;
@@ -124,11 +139,11 @@ always @(posedge clk_i) begin
                     state_PBCH_DMRS <= 3;
                 end
                 PBCH_DMRS_cnt <= PBCH_DMRS_cnt + 1;
-                if (PBCH_DMRS_cnt[0] == 0) PBCH_DMRS[PBCH_DMRS_cnt >> 1][0] <= (lfsr_out_0 ^ lfsr_out_1);
-                else                       PBCH_DMRS[PBCH_DMRS_cnt >> 1][1] <= (lfsr_out_0 ^ lfsr_out_1);
+                if (PBCH_DMRS_cnt[0] == 0) PBCH_DMRS[PBCH_DMRS_cnt >> 1][0] <= (lfsr_out_0 ^ LFSR_1[2].out);
+                else                       PBCH_DMRS[PBCH_DMRS_cnt >> 1][1] <= (lfsr_out_0 ^ LFSR_1[2].out);
 
-                if (PBCH_DMRS_cnt[0] == 0) debug_PBCH_DMRS_o[0] <= (lfsr_out_0 ^ lfsr_out_1);
-                else                       debug_PBCH_DMRS_o[1] <= (lfsr_out_0 ^ lfsr_out_1);
+                if (PBCH_DMRS_cnt[0] == 0) debug_PBCH_DMRS_o[0] <= (lfsr_out_0 ^ LFSR_1[2].out);
+                else                       debug_PBCH_DMRS_o[1] <= (lfsr_out_0 ^ LFSR_1[2].out);
                 debug_PBCH_DMRS_valid_o <= PBCH_DMRS_cnt[0];
             end
             3: begin
