54fef662e0692c2e2821cc72aa8c5827a0097dfa
Benjamin Menkuec <benjamin@menkuec.de>
2023-03-05 00:12:08 +0100

    make FFT_demod forward meta information (sfn, subframe_number, symbol_number)

--- hdl/FFT_demod.sv
+++ hdl/FFT_demod.sv
@@ -11,22 +11,46 @@ module FFT_demod #(
     localparam SFN_WIDTH = $clog2(SFN_MAX),
     localparam SUBFRAME_NUMBER_WIDTH = $clog2(SUBFRAMES_PER_FRAME - 1),
     localparam SYMBOL_NUMBER_WIDTH = $clog2(SYM_PER_SF - 1),
-    localparam USER_WIDTH = SFN_WIDTH + SUBFRAME_NUMBER_WIDTH + SYMBOL_NUMBER_WIDTH + $clog2(MAX_CP_LEN)
+    localparam USER_WIDTH_IN = SFN_WIDTH + SUBFRAME_NUMBER_WIDTH + SYMBOL_NUMBER_WIDTH + $clog2(MAX_CP_LEN),
+    localparam USER_WIDTH_OUT = SFN_WIDTH + SUBFRAME_NUMBER_WIDTH + SYMBOL_NUMBER_WIDTH + 1
 )
 (
     input                                       clk_i,
     input                                       reset_ni,
     input   wire       [IN_DW - 1 : 0]          s_axis_in_tdata,
-    input   wire       [USER_WIDTH - 1 : 0]     s_axis_in_tuser,    
+    input   wire       [USER_WIDTH_IN - 1 : 0]  s_axis_in_tuser,    
     input                                       s_axis_in_tlast,
     input                                       s_axis_in_tvalid,
     input                                       SSB_start_i,
     output  reg        [OUT_DW - 1 : 0]         m_axis_out_tdata,
-    output  reg        [1 : 0]                  m_axis_out_tuser,
+    output  reg        [USER_WIDTH_OUT -1  : 0] m_axis_out_tuser,
     output  reg                                 m_axis_out_tlast,
     output  reg                                 m_axis_out_tvalid,
     output                                      PBCH_valid_o,
     output                                      SSS_valid_o
+);
+
+// frame information (sfn, subframe number, symbol_number) go into this FIFO because FFT_core does not have tuser
+reg meta_FIFO_valid_in;
+wire meta_FIFO_valid_out;
+reg meta_FIFO_out_ready;
+wire [SFN_WIDTH + SUBFRAME_NUMBER_WIDTH + SYMBOL_NUMBER_WIDTH - 1 : 0] meta_FIFO_out_data;
+AXIS_FIFO #(
+    .DATA_WIDTH(SFN_WIDTH + SUBFRAME_NUMBER_WIDTH + SYMBOL_NUMBER_WIDTH),
+    .FIFO_LEN(16),
+    .USER_WIDTH(0),
+    .ASYNC(0)
+)
+meta_FIFO_i(
+    .clk_i(clk_i),
+    .reset_ni(reset_ni),
+
+    .s_axis_in_tdata(s_axis_in_tuser[USER_WIDTH_IN - 1 : $clog2(MAX_CP_LEN)]),
+    .s_axis_in_tvalid(meta_FIFO_valid_in),
+
+    .m_axis_out_tdata(meta_FIFO_out_data),
+    .m_axis_out_tready(meta_FIFO_out_ready),
+    .m_axis_out_tvalid(meta_FIFO_valid_out)
 );
 
 
@@ -49,36 +73,44 @@ always @(posedge clk_i) begin
         in_cnt <= '0;
         current_CP_len <= 18;
         CP_cnt <= HALF_CP_ADVANCE ? 9 : 0;
-    end else if (state_in == STATE_IN_SKIP_CP) begin // skip CP
-        if (s_axis_in_tvalid) begin
-            in_cnt <= '0;
-            if (CP_cnt == (current_CP_len - 1)) begin
-                state_in <= STATE_IN_PROCESS_SYMBOL;
-                CP_cnt <= '0;
-            end else begin
-                CP_cnt <= CP_cnt + 1;
+        meta_FIFO_valid_in <= '0;
+        in_data_f <= '0;
+        in_valid_f <= '0;
+    end else begin
+        if (state_in == STATE_IN_SKIP_CP) begin // skip CP
+            if (s_axis_in_tvalid) begin
+                in_cnt <= '0;
+                if (CP_cnt == (current_CP_len - 1)) begin
+                    state_in <= STATE_IN_PROCESS_SYMBOL;
+                    CP_cnt <= '0;
+                end else begin
+                    CP_cnt <= CP_cnt + 1;
+                end
             end
-        end
-    end else if (state_in == STATE_IN_PROCESS_SYMBOL) begin // process symbol
-        if (s_axis_in_tvalid) begin
-            if (in_cnt != (FFT_LEN - 1)) begin
-                in_cnt <= in_cnt + 1;
-            end else if (s_axis_in_tlast) begin
-                state_in <= STATE_IN_SKIP_CP;
-                CP_cnt <= HALF_CP_ADVANCE ? current_CP_len - 9 : 0;
-            end else begin
-                state_in <= STATE_IN_SKIP_END;
-                CP_cnt <= HALF_CP_ADVANCE ? current_CP_len - 9 : 0;
-            end
-        end
-    end else if (state_in == STATE_IN_SKIP_END) begin // skip repetition at end of symbol
-        if (s_axis_in_tvalid && s_axis_in_tlast) state_in <= STATE_IN_SKIP_CP;
-    end
+        end else if (state_in == STATE_IN_PROCESS_SYMBOL) begin // process symbol
+            if (s_axis_in_tvalid && (in_cnt == FFT_LEN - 2))  meta_FIFO_valid_in <= 1;
+            else                                              meta_FIFO_valid_in <= '0;
 
-    if (s_axis_in_tvalid) begin
-        current_CP_len <= s_axis_in_tuser[$clog2(MAX_CP_LEN) - 1 : 0];
-        in_data_f <= s_axis_in_tdata;
-        in_valid_f <= s_axis_in_tvalid;
+            if (s_axis_in_tvalid) begin
+                if (in_cnt != (FFT_LEN - 1)) begin
+                    in_cnt <= in_cnt + 1;
+                end else if (s_axis_in_tlast) begin
+                    state_in <= STATE_IN_SKIP_CP;
+                    CP_cnt <= HALF_CP_ADVANCE ? current_CP_len - 9 : 0;
+                end else begin
+                    state_in <= STATE_IN_SKIP_END;
+                    CP_cnt <= HALF_CP_ADVANCE ? current_CP_len - 9 : 0;
+                end
+            end
+        end else if (state_in == STATE_IN_SKIP_END) begin // skip repetition at end of symbol
+            if (s_axis_in_tvalid && s_axis_in_tlast) state_in <= STATE_IN_SKIP_CP;
+        end
+
+        if (s_axis_in_tvalid) begin
+            current_CP_len <= s_axis_in_tuser[$clog2(MAX_CP_LEN) - 1 : 0];
+            in_data_f <= s_axis_in_tdata;
+            in_valid_f <= s_axis_in_tvalid;
+        end
     end
 end
 
@@ -96,6 +128,7 @@ reg SSS_valid;
 reg PBCH_symbol;
 reg last_SC;
 reg [$clog2(FFT_LEN) - 1 : 0] out_cnt;
+reg [USER_WIDTH_OUT - 1 : 0] meta_out;
 wire valid_SC = (out_cnt >= SC_START) && (out_cnt <= SC_END - 1);
 wire valid_SSS_SC = (out_cnt >= SSS_START) && (out_cnt <= SSS_START + SSS_LEN - 1);
 always @(posedge clk_i) begin
@@ -109,6 +142,10 @@ always @(posedge clk_i) begin
     end else begin
         if (fft_val) begin
             last_SC <= (out_cnt == (FFT_LEN - 1 - 2 * SC_START));
+
+            PBCH_symbol <= (current_out_symbol == 0);
+            meta_FIFO_out_ready <= out_cnt == 0;
+            if (meta_FIFO_valid_out)  meta_out <= {meta_FIFO_out_data, current_out_symbol == 0};
             
             if (out_cnt == (FFT_LEN - 1)) begin
                 if (current_out_symbol == SYMS_BTWN_SSB - 1) begin
@@ -124,7 +161,6 @@ always @(posedge clk_i) begin
 
             PBCH_valid <= valid_SC && (current_out_symbol == 0);
             SSS_valid  <= valid_SSS_SC && (current_out_symbol == 1);
-            PBCH_symbol <= (current_out_symbol == 0);
         end else begin
             PBCH_valid <= '0;
             SSS_valid <= '0;
@@ -169,6 +205,7 @@ if (HALF_CP_ADVANCE) begin
     reg [MULT_DELAY - 1 : 0] SSS_valid_delay;
     reg [MULT_DELAY - 1 : 0] last_SC_delay;
     reg [MULT_DELAY - 1 : 0] PBCH_symbol_delay;
+    reg [USER_WIDTH_OUT - 1 : 0] meta_delay [MULT_DELAY - 1 : 0];
 
     reg [OUT_DW - 1 : 0] coeff [0 : 2**NFFT - 1];
     reg [NFFT - 1 : 0] coeff_idx;
@@ -177,9 +214,10 @@ if (HALF_CP_ADVANCE) begin
     reg SSS_valid_f;
     reg last_SC_f;
     reg PBCH_symbol_f;
+    reg [USER_WIDTH_OUT - 1 : 0] meta_delay_f;
     assign PBCH_valid_o = PBCH_valid_f;
     assign SSS_valid_o = SSS_valid_f;
-    assign m_axis_out_tuser = PBCH_symbol_f;
+    assign m_axis_out_tuser = meta_delay_f;
 
     initial begin
         real PI = 3.1415926535;
@@ -194,7 +232,6 @@ if (HALF_CP_ADVANCE) begin
             angle_acc = angle_acc + angle_step;
         end
     end
-
 
     complex_multiplier #(
         .OPERAND_WIDTH_A(OUT_DW / 2),
@@ -224,6 +261,7 @@ if (HALF_CP_ADVANCE) begin
             PBCH_valid_delay <= '0;
             SSS_valid_delay <= '0;
             last_SC_delay <= '0;
+            for (integer i = 0; i < MULT_DELAY; i = i + 1) meta_delay[i] <= '0;
             SSS_valid_f <= '0;
             PBCH_valid_f <= '0;
             last_SC_f <= '0;
@@ -235,16 +273,19 @@ if (HALF_CP_ADVANCE) begin
             PBCH_valid_delay[0] <= PBCH_valid;
             last_SC_delay[0] <= last_SC;
             PBCH_symbol_delay[0] <= PBCH_symbol;
+            meta_delay[0] <= meta_out;
             for (integer i = 0; i < (MULT_DELAY - 1); i = i + 1) begin
                 SSS_valid_delay[i+1] <= SSS_valid_delay[i];
                 PBCH_valid_delay[i+1] <= PBCH_valid_delay[i];
                 last_SC_delay[i+1] <= last_SC_delay[i];
                 PBCH_symbol_delay[i+1] <= PBCH_symbol_delay[i];
+                meta_delay[i+1] <= meta_delay[i];
             end
             SSS_valid_f <= SSS_valid_delay[MULT_DELAY - 1];
             PBCH_valid_f <= PBCH_valid_delay[MULT_DELAY - 1];
             last_SC_f <= last_SC_delay[MULT_DELAY - 1];
             PBCH_symbol_f <= PBCH_symbol_delay[MULT_DELAY - 1];
+            meta_delay_f <= meta_delay[MULT_DELAY - 1];
 
             if (fft_sync) coeff_idx <= '0;
             else coeff_idx <= coeff_idx + 1;
@@ -263,7 +304,7 @@ end else begin
         end else begin
             m_axis_out_tdata <= {fft_result_im, fft_result_re};
             m_axis_out_tlast <= last_SC;
-            m_axis_out_tuser <= PBCH_symbol;
+            m_axis_out_tuser <= meta_out;
             m_axis_out_tvalid <= fft_val && valid_SC;
         end
     end
