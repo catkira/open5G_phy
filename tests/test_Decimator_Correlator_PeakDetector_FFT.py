49c4bd6b4528cc88ec5ad15fcac641e5d81fad07
Benjamin Menkuec <benjamin@menkuec.de>
2023-04-04 23:11:55 +0200

    fix test_Decimator_Correlator_PeakDetector_FFT.py

--- tests/test_Decimator_Correlator_PeakDetector_FFT.py
+++ tests/test_Decimator_Correlator_PeakDetector_FFT.py
@@ -98,7 +98,7 @@ async def simple_test(dut):
         elif tb.MULT_REUSE == 2:
             DETECTOR_LATENCY = 29  # ok with new PSS_correlator_mr
         elif tb.MULT_REUSE == 4:
-            DETECTOR_LATENCY = 30 + 827  # ok with new PSS_correlator_mr
+            DETECTOR_LATENCY = 29 + 827  # ok with new PSS_correlator_mr
         elif tb.MULT_REUSE == 8:
             DETECTOR_LATENCY = 37 + 827  # ok with new PSS_correlator_mr
         elif tb.MULT_REUSE == 16:
@@ -394,4 +394,4 @@ def test(IN_DW, OUT_DW, TAP_DW, ALGO, WINDOW_LEN, HALF_CP_ADVANCE, NFFT, USE_TAP
 if __name__ == '__main__':
     os.environ['PLOTS'] = "1"
     # os.environ['SIM'] = 'verilator'
-    test(IN_DW = 32, OUT_DW = 32, TAP_DW = 32, ALGO = 0, WINDOW_LEN = 8, HALF_CP_ADVANCE = 1, NFFT = 8, USE_TAP_FILE = 1, MULT_REUSE = 8)
+    test(IN_DW = 32, OUT_DW = 32, TAP_DW = 32, ALGO = 0, WINDOW_LEN = 8, HALF_CP_ADVANCE = 1, NFFT = 8, USE_TAP_FILE = 1, MULT_REUSE = 4)
