beade0510e7b92fb47033571f1402369a5073778
Benjamin Menkuec <benjamin@menkuec.de>
2023-04-10 18:55:03 +0200

    add timestamp to output packet of ressource_grid_subscriber

--- hdl/ressource_grid_subscriber.sv
+++ hdl/ressource_grid_subscriber.sv
@@ -96,51 +96,61 @@ wire [SUBFRAME_NUMBER_WIDTH - 1 : 0] subframe_id = s_axis_iq_tuser[SUBFRAME_NUMB
 wire [SFN_WIDTH - 1 : 0] sfn = s_axis_iq_tuser[USER_WIDTH - 1 -: SFN_WIDTH];
 reg [IQ_WIDTH - 1 : 0] sample_buffer;
 reg [SAMPLE_ID_WIDTH - 1 : 0] sample_id_buffer;
+localparam NUM_TIMESTAMP_SAMPLES = 64 / IQ_WIDTH;
+reg [$clog2(NUM_TIMESTAMP_SAMPLES) - 1 : 0] timestamp_cnt;
+
 always @(posedge clk_i) begin
     if (!reset_ni) begin
         state <= '0;
-        out_fifo_ready <= '0;
         m_axis_fifo_tdata <= '0;
         m_axis_fifo_tvalid <= '0;
         sample_buffer <= '0;
         sample_id_ready <= '0;
+        timestamp_cnt <= '0;
+        out_fifo_ready <= '0;
     end else begin
         case (state)
-            0 : begin // enable FIFO output
-                out_fifo_ready <= 1;
-                state <= 1;
-            end
-            1 : begin // output blk_exp
+            0 : begin
                 if (out_fifo_valid) begin
+                    // start outputting data when out_fifo has data
+                    // output blk_exp first
                     m_axis_fifo_tdata <= blk_exp;
-                    sample_buffer <= out_fifo_data;
+                    m_axis_fifo_tvalid <= 1;
+                    state <= 1;
+                end else begin
+                    m_axis_fifo_tvalid <= '0;
+                end
+            end
+            1 : begin // output timestamp
+                if (sample_id_valid) begin
+                    m_axis_fifo_tdata <= sample_id_data;
+                    sample_id_buffer <= sample_id_data;
+                    timestamp_cnt <= 1;
                     m_axis_fifo_tvalid <= 1;
                     sample_id_ready <= 1;
-                    out_fifo_ready <= 0; // next cycle still has a valid sample coming out!
                     state <= 2;
                 end else begin
                     m_axis_fifo_tvalid <= '0;
                 end
             end
-            2 : begin // output stored sample
-                sample_id_buffer <= sample_id_data;  // TODO: insert this in output packet
-                sample_id_ready <= 0;
-                sample_buffer <= out_fifo_data;
-                m_axis_fifo_tdata <= sample_buffer;
+            2: begin // output remaining samples with timestamp
+                sample_id_ready <= '0;
+                m_axis_fifo_tdata <= sample_id_buffer;
+                sample_id_buffer <= sample_id_buffer >> IQ_WIDTH;
                 m_axis_fifo_tvalid <= 1;
-                out_fifo_ready <= 1; // start requesting new samples
-                state <= 3;
+                if (timestamp_cnt == NUM_TIMESTAMP_SAMPLES - 1) begin
+                    out_fifo_ready <= 1;
+                    state <= 3;
+                end else begin
+                    timestamp_cnt <= timestamp_cnt + 1;
+                end
             end
-            3: begin // output 2nd stored sample, new samples do not arrive yet
-                m_axis_fifo_tdata <= sample_buffer;
-                m_axis_fifo_tvalid <= 1;
-                state <= 4;
-            end
-            4 : begin // forward data from FIFO
+            3 : begin // output IQ samples (forward from FIFO)
                 m_axis_fifo_tdata <= out_fifo_data;
                 m_axis_fifo_tvalid <= out_fifo_valid;
                 if (out_fifo_last) begin
-                    state <= 1;
+                    state <= 0;
+                    out_fifo_ready <= '0;
                 end
             end
         endcase
